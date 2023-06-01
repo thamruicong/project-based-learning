fa0923b34c5137fcff3dac71d9859f528b84fa66
thamruicong <e0726692@u.nus.edu>
2023-06-02 00:21:49 +0800

    echo handler

--- TelegramGPT/main.py
+++ TelegramGPT/main.py
@@ -1,6 +1,6 @@
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import filters, ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes
 
 logging.basicConfig(
     format='%(asctime)s : %(name)s : %(levelname)s : %(message)s',
@@ -16,10 +16,15 @@ def getAPIKey():
 async def _start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text='Hello World!')
 
+async def _echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(getAPIKey()).build()
 
-    startHandler = CommandHandler('start', _start)
-    application.add_handler(startHandler)
+    start_handler = CommandHandler('start', _start)
+    echo_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, _echo)
+
+    application.add_handlers([start_handler, echo_handler])
 
     application.run_polling()
