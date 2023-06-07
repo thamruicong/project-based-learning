a80ee668efaf30127cf7f2bc533a52bcbf6deb44
thamruicong <e0726692@u.nus.edu>
2023-06-08 00:11:08 +0800

    google 2.0 handler

--- TelegramGPT/handlers.py
+++ TelegramGPT/handlers.py
@@ -1,5 +1,8 @@
 from telegram import Update
-from telegram.ext import CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import CommandHandler, MessageHandler, filters, ContextTypes, ConversationHandler
+import logging
+
+log = logging.getLogger(__name__)
 
 async def _start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text='Hello World!')
@@ -7,8 +10,45 @@ async def _start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def _echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
+async def _invalid_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text='Invalid command!')
+
+async def _invalid_input(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text='Invalid input!')
+
+GOOGLE = 0
+async def _start_google(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = 'Welcome to Google 2.0!\nThis is just a simple ChatGPT interface.\n\nType /cancel to cancel.'
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+
+    return GOOGLE
+
+async def _google(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = 'Google'
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+
+    return GOOGLE
+
+async def _cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log.info('User %s stopped Google 2.0', update.effective_user.username)
+    text = 'Google 2.0 Shutting down...'
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+
+    return ConversationHandler.END
+
 def getHandlers():
     start_handler = CommandHandler('start', _start)
     echo_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, _echo)
 
-    return [start_handler, echo_handler]
+    # Does not work correctly, need to disable all other handlers while in google_handler mode
+    google_handler = ConversationHandler(
+        entry_points=[CommandHandler('google', _start_google)],
+        states={
+            GOOGLE: [MessageHandler(filters.TEXT, _google)]
+        },
+        fallbacks=[CommandHandler('cancel', _cancel), MessageHandler(filters.ALL, _invalid_input)]
+    )
+
+    invalid_command_handler = MessageHandler(filters.COMMAND, _invalid_command)
+
+    return [start_handler, echo_handler, invalid_command_handler]
