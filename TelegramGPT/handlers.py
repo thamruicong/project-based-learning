1685e590c4cb6a7c291818cacb7b2b8d2c44c647
thamruicong <e0726692@u.nus.edu>
2023-06-11 11:06:57 +0800

    chatbot conversation handler

--- TelegramGPT/handlers.py
+++ TelegramGPT/handlers.py
@@ -7,13 +7,19 @@ from generate import query
 async def _help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=utils.HELP_TEXT)
 
+def decorator_help(func):
+    async def wrapper(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await func(update, context)
+        await _help(update, context)
+    return wrapper
+
+@decorator_help
 async def _invalid_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=utils.INVALID_COMMAND_TEXT)
-    await _help(update, context)
 
+@decorator_help
 async def _invalid_input(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=utils.INVALID_INPUT_TEXT)
-    await _help(update, context)
 
 async def _cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=utils.CANCELLED_TEXT)
@@ -29,9 +35,9 @@ async def _start_google(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     return utils.GOOGLE
 
+@decorator_help
 async def _google_help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=utils.GOOGLE_HELP_TEXT)
-    await _help(update, context)
 
 async def _google(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply = query(update.effective_message.text)
@@ -43,12 +49,21 @@ async def _start_chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     return utils.CHAT
 
+@decorator_help
 async def _chat_help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     pass
 
-async def _chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = 'Chat'
+async def _chat_girl_1(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = 'Chat girl 1'
     await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+
+    return utils.CHAT_GIRL_1
+
+async def _chat_custom(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = 'Chat custom'
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+
+    return utils.CHAT_CUSTOM
 
 ############################ GAME ############################
 async def _start_game(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -56,6 +71,7 @@ async def _start_game(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     return utils.GAME
 
+@decorator_help
 async def _game_help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     pass
 
@@ -65,12 +81,25 @@ async def _game(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 def getHandlers():
     utils.init_states()
+
+    chat_handler = ConversationHandler(
+        entry_points=[CommandHandler('girl1', _chat_girl_1), CommandHandler('custom', _chat_custom)],
+        states={
+            utils.CHAT_GIRL_1: [],
+            utils.CHAT_CUSTOM: [],
+        },
+        fallbacks=[CommandHandler('cancel', _cancel)],
+        map_to_parent={
+            ConversationHandler.END : ConversationHandler.END
+        }
+    )
+
     entry_points = [CommandHandler('google', _start_google), CommandHandler('chat', _start_chat), CommandHandler('game', _start_game)]
     main_handler = ConversationHandler(
         entry_points=entry_points,
         states={
             utils.GOOGLE: [MessageHandler(filters.TEXT & ~filters.COMMAND, _google), CommandHandler('help', _google_help)],
-            utils.CHAT: [MessageHandler(filters.TEXT & ~filters.COMMAND, _chat), CommandHandler('help', _chat_help)],
+            utils.CHAT: [chat_handler, CommandHandler('help', _chat_help)],
             utils.GAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, _game), CommandHandler('help', _game_help)],
         },
         fallbacks=[CommandHandler('cancel', _cancel)] + entry_points
--- TelegramGPT/utils.py
+++ TelegramGPT/utils.py
@@ -8,15 +8,19 @@ INVALID_COMMAND_TEXT = 'Invalid command!'
 INVALID_INPUT_TEXT = 'Invalid input!'
 CANCELLED_TEXT = 'Cancelled!'
 START_GOOGLE_TEXT = 'Welcome to Google 2.0!\nI will try my best to answer all your questions here!\n\nType /cancel to cancel.'
-START_CHAT_TEXT ='Welcome chat'
+START_CHAT_TEXT ='Welcome to Chat feature!\n\n/girl1 - basic girl chatbot\n/custom - custom chatbot\n\nType /cancel to cancel.'
 START_GAME_TEXT = 'Welcome game'
 GOOGLE_HELP_TEXT = 'Start by typing a question!\n\nEg. "What is the meaning of life?" or "Why is the sky blue?"\n\nType /cancel to cancel.'
 
 def init_states():
     states = ['GOOGLE', 'CHAT', 'GAME']
+    chat_states = ['CHAT_GIRL_1', 'CHAT_CUSTOM']
     
     for val, state in enumerate(states):
         globals()[state] = val
+    
+    for val, chat_state in enumerate(chat_states):
+        globals()[chat_state] = val
 
 def getTelegramToken():
     return os.getenv('TELEGRAM_TOKEN')
