dad08ee908b7f05d68760c99a87a132d2452b000
thamruicong <ruicongtham@gmail.com>
2023-08-08 18:26:59 +0800

    Re-init Engine and UI folders

--- RPG_Game/RPG_Game_UI/App.xaml.cs
+++ RPG_Game/RPG_Game_UI/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace RPG_Game
+namespace RPG_Game_UI
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- RPG_Game/RPG_Game_UI/MainWindow.xaml.cs
+++ RPG_Game/RPG_Game_UI/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace RPG_Game
+namespace RPG_Game_UI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
